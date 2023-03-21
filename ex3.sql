7afdb6a6dcadb9e1c06b1b83481d1c5289d0e35e
felipe based mito <42080804+felipeyousoro@users.noreply.github.com>
2023-03-20 23:00:06 -0300

    sql

--- ex3.sql
+++ ex3.sql
@@ -37,13 +37,13 @@ BEGIN;
 		END;
 	$$ LANGUAGE plpgsql;	
 	
-	CREATE OR REPLACE FUNCTION Get_Shortened_Middle_Name(_name VARCHAR) RETURNS VARCHAR AS $$
+	CREATE OR REPLACE FUNCTION Book_Loans.Get_Shortened_Middle_Name(_name VARCHAR) RETURNS VARCHAR AS $$
 		BEGIN
 			RETURN SUBSTRING(SPLIT_PART(_name, ' ', 2), 1, 1);
 		END;
 	$$ LANGUAGE plpgsql;
 
-	CREATE OR REPLACE FUNCTION Get_Shortened_Name(_name VARCHAR) RETURNS VARCHAR AS $$
+	CREATE OR REPLACE FUNCTION Book_Loans.Get_Shortened_Name(_name VARCHAR) RETURNS VARCHAR AS $$
 		DECLARE 
 			_first_name VARCHAR;
 			_middle_name VARCHAR;
@@ -51,26 +51,43 @@ BEGIN;
 		BEGIN
 			SELECT INTO _first_name, _middle_name, _surname
 				SPLIT_PART(_name, ' ', 1),
-				Get_Shortened_Middle_Name(_name),
+				Book_Loans.Get_Shortened_Middle_Name(_name),
 				SPLIT_PART(_name, ' ', 3);
 			RETURN _first_name || ' ' || _middle_name || '. ' || _surname;
 
 		END;
 	$$ LANGUAGE plpgsql;
 	
-	CREATE TEMPORARY TABLE Book_Authors_Log (
+	CREATE TABLE Book_Loans.Book_Authors_Log (
+		book_id INT NOT NULL,
 		new_author_name VARCHAR(255) NOT NULL,
-		_count INT NOT NULL
+		old_author_name VARCHAR(255) NOT NULL,
+		update_timestamp TIMESTAMP NOT NULL DEFAULT NOW()
 	);
 
-	CREATE PROCEDURE Detect_And_Reconcile_Authors() LANGUAGE plpgsql AS $$
+	CREATE FUNCTION Book_Loans.Log_Author_Update() RETURNS TRIGGER AS $$
+		BEGIN
+			INSERT INTO Book_Loans.Book_Authors_Log (book_id, new_author_name, old_author_name)
+				VALUES (NEW.book_id, NEW.author_name, OLD.author_name);
+			RETURN NEW;
+		END;
+	$$ LANGUAGE plpgsql;
+
+	CREATE TRIGGER Log_Author_Update
+		AFTER UPDATE ON Book_Loans.Book_Authors
+		FOR EACH ROW
+		WHEN (OLD.author_name <> NEW.author_name)
+		EXECUTE PROCEDURE Book_Loans.Log_Author_Update();
+
+
+	CREATE PROCEDURE Book_Loans.Detect_And_Reconcile_Authors() LANGUAGE plpgsql AS $$
 		DECLARE
 			_record1 RECORD;
 			_record2 RECORD;
 			_author_name VARCHAR;
 			_count INT;
 			_count_sum INT;
-			_cursor1 CURSOR FOR SELECT author_name FROM Book_Authors FOR UPDATE;
+			_cursor1 CURSOR FOR SELECT author_name FROM Book_Loans.Book_Authors FOR UPDATE;
 		BEGIN 
 			OPEN _cursor1;
 			LOOP
@@ -79,46 +96,45 @@ BEGIN;
 				EXIT WHEN NOT FOUND;
 					SELECT author_name, COUNT(*) AS author_count
 						INTO _author_name, _count
-						FROM Book_Authors
+						FROM Book_Loans.Book_Authors
 						WHERE Levenshtein(author_name, _record1.author_name) < 2 OR
-							Levenshtein(Get_Shortened_Name(author_name), Get_Shortened_Name(_record1.author_name)) < 2 OR
-							Get_Shortened_Middle_Name(author_name) = Get_Shortened_Middle_Name(_record1.author_name)
+							Levenshtein(Book_Loans.Get_Shortened_Name(author_name), Book_Loans.Get_Shortened_Name(_record1.author_name)) < 2 OR
+							Book_Loans.Get_Shortened_Middle_Name(author_name) = Book_Loans.Get_Shortened_Middle_Name(_record1.author_name)
 						GROUP BY author_name
 						ORDER BY author_count DESC
 						LIMIT 1;
 						
 						SELECT SUM(_count) INTO _count_sum;
 
-					UPDATE Book_Authors 
+					UPDATE Book_Loans.Book_Authors 
 						SET author_name = _author_name
 						WHERE Levenshtein(author_name, _author_name) < 2 OR
-							Levenshtein(Get_Shortened_Name(author_name), Get_Shortened_Name(_record1.author_name)) < 2 OR
-							Get_Shortened_Middle_Name(author_name) = Get_Shortened_Middle_Name(_record1.author_name);
+							Levenshtein(Book_Loans.Get_Shortened_Name(author_name), Book_Loans.Get_Shortened_Name(_record1.author_name)) < 2 OR
+							Book_Loans.Get_Shortened_Middle_Name(author_name) = Book_Loans.Get_Shortened_Middle_Name(_record1.author_name);
 
-					INSERT INTO Book_Authors_Log (new_author_name, _count) VALUES (_record1.author_name, _count_sum);
 
 			END LOOP;
 			CLOSE _cursor1;
 		END;
 	$$;
 	
-	CALL Detect_And_Reconcile_Authors();
-	SELECT * FROM Book_Authors;
-	SELECT * FROM Book_Authors_Log;
+	CALL Book_Loans.Detect_And_Reconcile_Authors();
+	SELECT * FROM Book_Loans.Book_Authors;
+	SELECT * FROM Book_Loans.Book_Authors_Log;
 	
--- 		SELECT author_name, COUNT(*) AS author_count
--- 				FROM Book_Authors
--- 				WHERE Levenshtein(author_name, 'John J. Powell') < 2 OR
--- 					Levenshtein(Get_Shortened_Name(author_name), Get_Shortened_Name('John J. Powell')) < 2 OR --possivelmente and aqui
--- 					Get_Shortened_Middle_Name(author_name) = Get_Shortened_Middle_Name('John J. Powell')
--- 				GROUP BY author_name
--- 				ORDER BY author_count DESC;
+ 		SELECT author_name, COUNT(*) AS author_count
+ 				FROM Book_Loans.Book_Authors
+ 				WHERE Levenshtein(author_name, 'John J. Powell') < 2 OR
+ 					Levenshtein(Book_Loans.Get_Shortened_Name(author_name), Book_Loans.Get_Shortened_Name('John J. Powell')) < 2 OR --possivelmente and aqui
+ 					Book_Loans.Get_Shortened_Middle_Name(author_name) = Book_Loans.Get_Shortened_Middle_Name('John J. Powell')
+ 				GROUP BY author_name
+ 				ORDER BY author_count DESC;
 		
--- 		UPDATE Book_Authors 
--- 			SET author_name = 'John J. Powell'
--- 			WHERE Levenshtein(author_name, 'John J. Powell') < 2 OR
--- 			Levenshtein(Get_Shortened_Name(author_name), Get_Shortened_Name('John J. Powell')) < 2 OR
--- 			Get_Shortened_Middle_Name(author_name) = Get_Shortened_Middle_Name('John J. Powell');
+ 		UPDATE Book_Loans.Book_Authors 
+ 			SET author_name = 'John J. Powell'
+ 			WHERE Levenshtein(author_name, 'John J. Powell') < 2 OR
+ 			Levenshtein(Book_Loans.Get_Shortened_Name(author_name), Book_Loans.Get_Shortened_Name('John J. Powell')) < 2 OR
+ 			Book_Loans.Get_Shortened_Middle_Name(author_name) = Book_Loans.Get_Shortened_Middle_Name('John J. Powell');
 
 
 
