f9a12c129b9394315b7abd50e0045d2bc1eb3b4d
felipe based mito <42080804+felipeyousoro@users.noreply.github.com>
2023-03-26 13:50:09 -0300

    FINALMENTE ORACLE MAT VIEW RODANDO

--- Atividade 1/oracle/ex1.sql
+++ Atividade 1/oracle/ex1.sql
@@ -1,4 +1,5 @@
-CREATE MATERIALIZED VIEW Felipe.month_borrowers (
+CREATE MATERIALIZED VIEW Felipe.month_borrowers 
+    (
     card_no,
     name,
     address,
@@ -18,3 +19,35 @@ CREATE MATERIALIZED VIEW Felipe.month_borrowers (
         JOIN Felipe.Book bo ON bl.book_id = bo.book_id
         JOIN Felipe.Library_Branch lb ON bl.branch_id = lb.branch_id
         WHERE (bl.due_date - bl.date_out) > 30;
+
+-- eu simplesmente esqueci de fazer o update no pgsql então
+-- estou fazendo aqui, antes tarde do que nunca
+CREATE OR REPLACE TRIGGER Felipe.Update_Month_Borrowers
+	AFTER INSERT OR UPDATE OR DELETE ON Felipe.Book_Loans
+	DECLARE
+        PRAGMA AUTONOMOUS_TRANSACTION;
+	BEGIN 
+		DBMS_MVIEW.REFRESH('Felipe.Month_Borrowers');
+	END;
+
+	
+-- eu juro que tentei fazer direto na criação da view
+-- a parte de atualizar com REFRESH ON COMMIT
+-- mas deu algum problema por causa dos joins
+-- então eu fiz o trigger
+
+-- CREATE MATERIALIZED VIEW LOG ON Felipe.Book_Loans
+--     WITH PRIMARY KEY, ROWID, SEQUENCE (date_out, due_date)
+-- INCLUDING NEW VALUES;
+
+-- CREATE MATERIALIZED VIEW LOG ON Felipe.Book
+--     WITH PRIMARY KEY, ROWID, SEQUENCE (title, publisher_name)
+-- INCLUDING NEW VALUES;
+
+-- CREATE MATERIALIZED VIEW LOG ON Felipe.Borrower
+--     WITH PRIMARY KEY, ROWID, SEQUENCE (name, address, phone)
+-- INCLUDING NEW VALUES;
+
+-- CREATE MATERIALIZED VIEW LOG ON Felipe.Library_Branch
+--     WITH PRIMARY KEY, ROWID, SEQUENCE (branch_name, address)
+-- INCLUDING NEW VALUES;
--- Atividade 1/pgsql/ex1.sql
+++ Atividade 1/pgsql/ex1.sql
@@ -25,4 +25,16 @@ CREATE MATERIALIZED VIEW Book_Loans.month_borrowers (
         JOIN Book_Loans.Library_Branch lb ON bl.branch_id = lb.branch_id
         WHERE (bl.due_date - bl.date_out) > 30;
 
+    CREATE OR REPLACE FUNCTION Book_Loans.refresh_month_borrowers() RETURNS trigger AS $$
+        BEGIN
+            REFRESH MATERIALIZED VIEW Book_Loans.month_borrowers;
+            RETURN NULL;
+        END;
+    $$ LANGUAGE plpgsql;
+
+    CREATE TRIGGER refresh_month_borrowers
+        AFTER INSERT OR UPDATE OR DELETE ON Book_Loans.Book_Loans
+            FOR EACH STATEMENT
+                EXECUTE FUNCTION Book_Loans.refresh_month_borrowers();
+
 ROLLBACK;
